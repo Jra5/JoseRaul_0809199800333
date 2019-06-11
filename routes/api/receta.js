1b6f7e25154e39310b791cc9520a70460c533544
JRaúl <almendaresraul10@gmail.com>
2019-06-11 11:30:00 -0600

    Método GET

--- routes/api/receta.js
+++ routes/api/receta.js
@@ -28,5 +28,31 @@ ColeccionReceta.push(
         })
 );
 
+//METODOS POSTMAN
+
+//GET 
+router.get('/', (req, res, next) => {
+    res.status(200).json(ColeccionReceta);
+});
+
+//GET obteniéndolo mediante ID
+router.get('/:id', (req, res, next) => {
+    if (!req.params.id) return next;
+
+    var id = req.params.id;
+    var Receta = ColeccionReceta.filter((e,i) =>{
+        return (e.id === id);
+    });
+
+    if (Receta.lenght > 0){
+        res.status(200).json(Receta[0]);
+    }
+    else{
+        res.status(403).json({});
+    }
+});
+
+
+
 
 module.exports = router;
